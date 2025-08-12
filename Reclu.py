86b62bbb4177bdec6c10802e6706e01bc716046b
Luis Ríos <141874238+LuisHRiosC18@users.noreply.github.com>
2025-08-12 12:14:23 -0600

    Create Reclu.py
    Pagina de bienvenida

---
+++ Reclu.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Reclutamiento",
+    page_icon="👋",
+)
+
+st.write("# Bienvenido a la pagina dedicada a Reclutamiento! 🙂‍↔️🙂‍↔️🙂‍↔️🙂‍↔️🙂‍↔️")
+
+st.sidebar.success("Escoge la información que quieras ver.")
+
+st.markdown(
+    """
+    En esta aplicación encontrarás información relacionada a reclutamiento de nuestra empresa.
+    ### ¿Qué incluye? 🔥🔥🔥🔥🔥🔥🔥
+    - Métricas del trabajo realizado por los reclutadores. 🦾🦾🦾
+    - Comparativas entre los distintos equipos de reclutadores. 🫂🫂🫂
+    - Pruebas de Hipotesis que comparan el desempeño a comparación del desempeño historico. ⚖️⚖️⚖️
+)
