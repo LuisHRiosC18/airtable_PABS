ecbe361a5d9bd57d7fe3466cb7c128d9e9661aa7
Luis Ríos <141874238+LuisHRiosC18@users.noreply.github.com>
2025-08-14 11:38:49 -0600

    Update ⚔️Comparativa.py

--- pages/⚔️Comparativa.py
+++ pages/⚔️Comparativa.py
@@ -1,1 +1,17 @@
 ## Esta pagina es dedicada a la comparativa entre el desempeño de los distintos equipos.
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+from datetime import datetime, timedelta
+from pyairtable import Api
+import numpy as np
+
+st.set_page_config(
+    page_title="Comparativa entre equipos ",
+    page_icon="⚔️",
+    layout="wide"
+)
+
+st.title("⚔️ Comparativa entre equipos de  ")
+st.markdown("Visualiza el desempeño de cada reclutador con respecto a la semana, mes e historicamente.")
+
